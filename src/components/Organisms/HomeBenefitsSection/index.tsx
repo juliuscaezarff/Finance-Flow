08c13106db4a2fe0ea856b5240ea5735b1189cea
Julius Caezar <julius.caezar25@gmail.com>
2023-09-07 14:11:22 -0300

    feat: criando a section de beneficios

---
+++ src/components/Organisms/HomeBenefitsSection/index.tsx
@@ -0,0 +1,9 @@
+import { CompanySection } from "@/components/Molecules/CompanySection"
+
+export const HomeBenefitsSection = () => {
+  return (
+    <section className="container">
+      <CompanySection />
+    </section>
+  )
+}
