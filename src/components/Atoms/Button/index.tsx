ce11cb9a484a56dea8adcd9383f30fd8270190f7
Julius Caezar <julius.caezar25@gmail.com>
2023-09-07 14:07:17 -0300

    feat: passando a className dinamicamente no component

--- src/components/Atoms/Button/index.tsx
+++ src/components/Atoms/Button/index.tsx
@@ -1,4 +1,4 @@
-import { cn } from '@/app/lib/utils'
+import { cn } from '@/lib/utils'
 import { ButtonHTMLAttributes } from 'react'
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
@@ -7,7 +7,7 @@ export const Button = ({ children, className, ...props }: ButtonProps) => {
   return (
     <button
       className={cn(
-        'bg-blue-primary-light py-[27px] px-[32px] h-[56px] flex items-center justify-center rounded-[80px] uppercase text-white text-base font-bold',
+        'bg-blue-primary-light py-[27px] px-[32px] h-[56px] flex items-center justify-center rounded-[80px] uppercase text-white text-base font-bold font-sans not-italic leading-4 tracking-widest',
         className
       )}
       {...props}
