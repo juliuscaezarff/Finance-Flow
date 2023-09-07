5a67ebc8ecccc662ca30b1dab7464d54cbbcc392
Julius Caezar <julius.caezar25@gmail.com>
2023-09-07 14:05:50 -0300

    feat: configurando a font do projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,10 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { DM_Sans } from 'next/font/google'
 
-const Sans = DM_Sans({ subsets: ['latin'] })
+const Sans = DM_Sans({
+  variable: '--font-sans',
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
   title: 'FinanceFlow',
@@ -17,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={Sans.className}>
+      <body className={Sans.variable}>
         <Header />
         {children}
       </body>
