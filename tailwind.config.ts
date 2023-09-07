69e7dfcab07173f746cfeeb51047d998063f2403
Julius Caezar <julius.caezar25@gmail.com>
2023-09-07 14:12:51 -0300

    feat: configurando a font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,15 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    fontFamily: {
+      sans: ['var(--font-sans)', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-radial-mobile': "url(/assets/Vector.png)"
       },
       colors: {
         'blue-primary': '#010D50',
