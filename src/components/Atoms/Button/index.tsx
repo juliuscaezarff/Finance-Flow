b27286ed06066e748f6bfe3137127ef454f2f003
Julius Caezar <julius.caezar25@gmail.com>
2023-09-09 12:44:39 -0300

    feat: modificando estilido do botão

--- src/components/Atoms/Button/index.tsx
+++ src/components/Atoms/Button/index.tsx
@@ -7,7 +7,7 @@ export const Button = ({ children, className, ...props }: ButtonProps) => {
   return (
     <button
       className={cn(
-        'bg-blue-primary-light py-[27px] px-[32px] h-[56px] flex items-center justify-center rounded-[80px] uppercase text-white text-base font-bold font-sans not-italic leading-4 tracking-widest',
+        'bg-blue-primary-light py-[27px] px-[32px] h-[56px] flex items-center justify-center rounded-[80px] uppercase text-white text-base font-bold font-sans not-italic leading-[16px] tracking-widest',
         className
       )}
       {...props}
