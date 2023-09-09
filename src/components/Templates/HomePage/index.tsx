38c6bbd426a25facf72518de413efb7f04ba0b28
Julius Caezar <julius.caezar25@gmail.com>
2023-09-09 13:18:50 -0300

    feat: adicionando 2 novas seções da página home

--- src/components/Templates/HomePage/index.tsx
+++ src/components/Templates/HomePage/index.tsx
@@ -1,11 +1,16 @@
 import { HomeHeroSection } from '@/components/Organisms/HomeHeroSection'
 import { HomeBenefitsSection } from '@/components/Organisms/HomeBenefitsSection'
+import { HomeRewardsTokenSection } from '@/components/Organisms/HomeRewardsTokenSection'
+import { ExploreAppSection } from '@/components/Molecules/ExploreAppSection'
 
 export function HomePage() {
   return (
     <>
       <HomeHeroSection />
       <HomeBenefitsSection />
+      <HomeRewardsTokenSection />
+      <ExploreAppSection />
+      <div>dadssadsad</div>
     </>
   )
 }
