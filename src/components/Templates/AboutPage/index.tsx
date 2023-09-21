e38a7b5e129ba154ffdb7a3dd32d86c9f88571db
Julius Caezar <julius.caezar25@gmail.com>
2023-09-21 17:23:16 -0300

    feat: criando o template da pagina about

---
+++ src/components/Templates/AboutPage/index.tsx
@@ -0,0 +1,11 @@
+import { AboutHeroSection } from '@/components/Organisms/AboutHeroSection'
+import { AboutOurMission } from '@/components/Organisms/AboutOurMission'
+
+export function AboutPage() {
+  return (
+    <>
+      <AboutHeroSection />
+      <AboutOurMission />
+    </>
+  )
+}
