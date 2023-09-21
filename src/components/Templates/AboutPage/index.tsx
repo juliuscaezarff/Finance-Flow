d50789da7fbf43691319c06f3124dec6258ae183
Julius Caezar <julius.caezar25@gmail.com>
2023-09-21 19:23:00 -0300

    feat: adicionando uma nova seção de timeline

--- src/components/Templates/AboutPage/index.tsx
+++ src/components/Templates/AboutPage/index.tsx
@@ -1,11 +1,13 @@
 import { AboutHeroSection } from '@/components/Organisms/AboutHeroSection'
 import { AboutOurMission } from '@/components/Organisms/AboutOurMission'
+import { TimeLineSection } from '@/components/Organisms/AboutTimeLineSection'
 
 export function AboutPage() {
   return (
     <>
       <AboutHeroSection />
       <AboutOurMission />
+      <TimeLineSection />
     </>
   )
 }
