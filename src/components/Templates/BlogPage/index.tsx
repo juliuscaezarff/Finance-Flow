746741fcb1e0b237dd00b56829f79e11591c593e
Julius Caezar <julius.caezar25@gmail.com>
2024-01-30 17:50:17 -0300

    feat: template da pagina blog

---
+++ src/components/Templates/BlogPage/index.tsx
@@ -0,0 +1,11 @@
+import { BlogHerosection } from "@/components/Organisms/BlogHeroSection";
+import { NewsletterSection } from "@/components/Organisms/BlogNewsletterSection";
+
+export function BlogPage( ) {
+  return (
+    <>
+      <BlogHerosection />
+      <NewsletterSection />
+    </>
+  )
+}
