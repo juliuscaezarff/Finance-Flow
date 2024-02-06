e6256a37b26c63c208c2ebc6bf660219d666b3f0
Julius Caezar <julius.caezar25@gmail.com>
2024-02-06 19:24:33 -0300

    refactor: correção no texto de introdução

--- src/components/Organisms/HomeBenefitsSection/index.tsx
+++ src/components/Organisms/HomeBenefitsSection/index.tsx
@@ -10,7 +10,7 @@ export const HomeBenefitsSection = () => {
         <CompanySection text='Finance flow has been featured on'/>
 
         <div className="lg:flex lg:flex-col lg:items-center lg:justify-center lg:w-[740px] mt-[64px]">
-          <h1 className="font-sans font-bold text-3xl w-[273px] lg:w-[534px] lg:text-[42px]">
+          <h1 className="font-sans font-bold text-3xl w-[273px] lg:w-[545px] lg:text-[42px]">
             Build your crypto portfolio
           </h1>
           <p className="font-sans text-base w-[314px] lg:w-[496px] lg:text-center mt-4">
